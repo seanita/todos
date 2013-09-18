1b2a0d204806368d46ad6b3bdacbe79ab7de3efb
Seanita Tolliver <seanitat@gmail.com>
2013-09-18 14:36:48 -0400

    Refactor manage todo spec

--- spec/integration/manage_todo_spec.rb
+++ spec/integration/manage_todo_spec.rb
@@ -4,16 +4,13 @@ feature 'Manage todos' do
 	scenario 'create a new todo' do
 		sign_in
 		create_todo_with_description('Buy some milk')
-
 		user_sees_todo_item 'Buy some milk'
 	end
 
 	scenario 'view only my todos' do
 		create(:todo, description: 'Buy some eggs', owner_email: 'not_me@example.com')
 		sign_in_as 'me@example.com'
-
 		create_todo_with_description 'Buy some milk'
-
 		user_sees_todo_item 'Buy some milk'
 		user_does_not_see_todo_item 'Buy some eggs'
 	end
@@ -21,11 +18,14 @@ feature 'Manage todos' do
 	scenario 'mark todos as complete' do
 		sign_in
 		create_todo_with_description 'Buy some milk'
-		
-		within 'li.todo' do
+		complete_todo 'Buy some milk'
+		user_sees_completed_todo_item 'Buy some milk'
+	end
+
+	def complete_todo(description)
+		within "li.todo:contains('#{description}')" do
 			click_link 'Complete'
 		end
-		expect(page).to have_selector('li.todo.completed')
 	end
 
 	def create_todo_with_description(description)
@@ -36,6 +36,10 @@ feature 'Manage todos' do
 
 	def user_sees_todo_item(description)
 		expect(page).to have_css('li.todo', text: description)
+	end
+
+		def user_sees_completed_todo_item(description)
+		expect(page).to have_css('li.todo.completed', text: description)
 	end
 
 	def user_does_not_see_todo_item(description)
